4d8837c45d46d8e0ab38e4b9ea421d95cf740330
Hal Peterson <vobine@gmail.com>
2014-08-10 21:45:06 +0000

    Zeroth cut at common code

---
+++ food-lib.php
@@ -0,0 +1,14 @@
+<?php
+
+function newSQL ($user, $pwd)
+{
+  return new PDO ('mysql:host=localhost;dbname=food', $user, $pwd);
+}
+
+function querySQL ($pdo, $sql, $parameters)
+{
+  $stmt = $pdo->prepare ($sql);
+  if ($stmt && $stmt->execute ($parameters)) {
+    // Success!
+}
+?>
