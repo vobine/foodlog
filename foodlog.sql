eb0775b8b40feaffd0e44555c5282ca49f61abd0
Hal Peterson <vobine@users.noreply.github.com>
2014-08-31 10:56:20 -0500

    First cut at data definitions

---
+++ foodlog.sql
@@ -0,0 +1,25 @@
+CREATE TABLE foodlog (
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    INDEX	  (id),
+    PRIMARY KEY   (id),
+
+    atype	  INT UNSIGNED NOT NULL,
+    FOREIGN KEY	  (atype_id) REFERENCES foodtype (id),
+    INDEX   	  (atype),
+
+    btype	  VARCHAR(80),
+    stamp 	  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    quantity  	  INT UNSIGNED,
+    comment	  VARCHAR(240)
+);
+
+CREATE TABLE foodtype (
+    id	    	INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    INDEX	(id),
+    PRIMARY KEY	(id),
+
+    nickName	CHAR(4),
+    INDEX	(nickName),
+
+    fullName	VARCHAR(80)
+);
