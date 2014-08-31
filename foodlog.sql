622d87b8d53cc148c06cbf2f99b39d5c93925e1a
Hal Peterson <vobine@gmail.com>
2014-08-31 20:03:36 +0000

    Define child table first. Also fix syntax.

--- foodlog.sql
+++ foodlog.sql
@@ -1,19 +1,4 @@
-USE foodlog;
-
-CREATE TABLE foodlog (
-    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    INDEX	  (id),
-    PRIMARY KEY   (id),
-
-    atype	  INT UNSIGNED NOT NULL,
-    FOREIGN KEY	  (atype_id) REFERENCES foodtype (id),
-    INDEX   	  (atype),
-
-    btype	  VARCHAR(80),
-    stamp 	  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    quantity  	  INT UNSIGNED,
-    comment	  VARCHAR(240)
-);
+USE food;
 
 CREATE TABLE foodtype (
     id	    	INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -24,4 +9,19 @@ CREATE TABLE foodtype (
     INDEX	(nickName),
 
     fullName	VARCHAR(80)
+);
+
+CREATE TABLE foodlog (
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    INDEX	  (id),
+    PRIMARY KEY   (id),
+
+    atype	  INT UNSIGNED NOT NULL,
+    FOREIGN KEY	  (atype) REFERENCES foodtype (id),
+    INDEX   	  (atype),
+
+    btype	  VARCHAR(80),
+    stamp 	  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    quantity  	  INT UNSIGNED,
+    comment	  VARCHAR(240)
 );
