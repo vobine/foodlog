21fd8e2b076c86207bd56b1eddacacdad991614b
Hal Peterson <vobine@users.noreply.github.com>
2016-08-03 22:45:30 -0500

    Display contents of event-type database

--- mt.py
+++ mt.py
@@ -1,3 +1,5 @@
 import foodlog.models as flm
 
 flm.init_db ('sqlite:///:memory:', verbose=True)
+for i, kind in enumerate (flm.session.query (flm.Kind)):
+    print ('    {0:2d} {1:s}'.format (i, str (kind)))
