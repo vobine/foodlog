11ace20b5ffc19a2bc8fd56045c4f6359672d7d4
Hal Peterson <vobine@users.noreply.github.com>
2018-07-26 17:06:37 -0500

    PIPification, round 1

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="foodlog",
+    version="0.1.0",
+    author="Hal Peterson",
+    author_email="vobine@gmail.com",
+    description="Food/diet/lifestyle logging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vobine/foodlog",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: ",
+        "Operating System :: OS Independent",
+    ),
+)
